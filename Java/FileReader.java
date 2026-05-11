dfd9329e8a8307a2c6f73fd9969029b9a1c945c7
ToNI <Simakovdima237@gmail.com>
2026-05-11 05:04:13 +0300

    Чтение файла и проверка по имени

---
+++ Java/FileReader.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class FileReader {
+    Scanner scanner = new Scanner(System.in);
+    Examination examination = new Examination();
+public void fileRead() {
+    System.out.println("Введите имя файла: ");
+    String filename = scanner.nextLine();
+    if(Files.exists(Paths.get(filename))) {
+        if (examination.isValidPath(filename)) {
+            try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+                System.out.println();
+                stream.forEach(System.out::println);
+            } catch (IOException e) {
+                System.out.println("Ошибка!");
+            }
+        }
+    }
+}
+}
