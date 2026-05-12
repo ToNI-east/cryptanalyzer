12495d3a5e7092a8176ac41a7da0939cc8287e41
ToNI <Simakovdima237@gmail.com>
2026-05-12 21:26:41 +0300

    Изменил расшифратор пофиксив баги

--- Java/Decoder.java
+++ Java/Decoder.java
@@ -13,9 +13,32 @@ public class Decoder {
             if (examination.isValidPath(fileName)) {
                 System.out.println("Введите ключ: ");
                 int key = scanner.nextInt();
+                key = Math.abs(key) % 26;
                 try {
                     for (char character : Files.readString(Paths.get(fileName), StandardCharsets.UTF_8).toCharArray()) {
-                        result.append((char) (character - key));
+                        if (Character.isUpperCase(character)) {
+                            if (character >= 'A' && character <= 'Z') {
+                                result.append((char) ((character - 'A' - key + 26) % 26 + 'A'));
+                            } else if (character >= 'А' && character <= 'Я') {
+                                result.append((char) ((character - 'А' - key + 32) % 32 + 'А'));
+                            } else if (character == 'Ё') {
+                                result.append((key % 2 == 0) ? 'Ё' : 'Е');
+                            } else {
+                                result.append(character);
+                            }
+                        } else if (Character.isLowerCase(character)) {
+                            if (character >= 'a' && character <= 'z') {
+                                result.append((char) ((character - 'a' - key + 26) % 26 + 'a'));
+                            } else if (character >= 'а' && character <= 'я') {
+                                result.append((char) ((character - 'а' - key + 32) % 32 + 'а'));
+                            } else if (character == 'ё') {
+                                result.append((key % 2 == 0) ? 'ё' : 'е');
+                            } else {
+                                result.append(character);
+                            }
+                        } else {
+                            result.append(character);
+                        }
                     }
                     Files.writeString(Paths.get(fileName), result.toString());
                     System.out.println("Файл успешно расшифрован! ");
